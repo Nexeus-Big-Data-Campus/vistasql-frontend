04a0feceb5dad2ca65bbce3023586f3cecb0c291
CrsithanL96 <crismachine96@gmail.com>
2025-05-19 19:50:01 +0200

    index on cristhianL96: e6a8e73 Servicios para hacer las peticiones al backend

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom'; // usa BrowserRouter en lugar de crear router dentro de App
 
-const rootElement = document.getElementById('root') as HTMLElement;
-const root = ReactDOM.createRoot(rootElement);
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 root.render(
   <React.StrictMode>
-    <App />
+     
   </React.StrictMode>
 );
