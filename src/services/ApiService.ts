6c2cb251bff5bda2c263d4543b9643178f41d05c
Daniel Alonso <Dasarts73@gmail.com>
2025-06-05 18:35:36 +0200

    bugs fixed

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -1,3 +1,11 @@
+type LoginResponse = {
+  token: string;
+  user: {
+    id: number;
+    username: string;
+    email: string;
+  };
+};
 export class ApiService {
   private API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
@@ -16,7 +24,7 @@ export class ApiService {
 
   private async makeRequest<T>(endpoint: string, method: string, body?: any, requireAuth: boolean = false): Promise<T> {
     const headers = requireAuth ? this.getAuthHeaders() : { "Content-Type": "application/json" };
-    
+
     const response = await fetch(`${this.API_URL}${endpoint}`, {
       method,
       headers: { "Content-Type": "application/json" },
@@ -32,15 +40,17 @@ export class ApiService {
     return await response.json();
   }
 
-  async login(email: string, password: string) {
-    return await this.makeRequest<{ message: string }>("/login", "POST", { email, password });
+  async login(email: string, password: string):  Promise<LoginResponse> {
+    return await this.makeRequest("/login", "POST", { email, password });
   }
 
   async signin(name: string, email: string, password: string) {
-    return await this.makeRequest<{ message: string }>("/signin", "POST", { name, email, password });
+    return await this.makeRequest("/signin", "POST", { name, email, password });
   }
 
   async getUserProfile() {
     return await this.makeRequest<any>("/profile", "GET", undefined, true); // requiere auth
   }
 }
+
+export const apiService = new ApiService();
